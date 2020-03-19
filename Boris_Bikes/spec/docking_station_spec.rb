075dab87d6b1b904f024482894dcb063192ace3f
Angelica <Angelica137@users.noreply.github.com>
2020-03-19 16:36:17 +0000

    add release working bike

--- Boris_Bikes/spec/docking_station_spec.rb
+++ Boris_Bikes/spec/docking_station_spec.rb
@@ -1,6 +1,14 @@
 require 'docking_station'
 
 describe DockingStation do
+
+# TEST: Release a bike
   it { is_expected.to respond_to :release_bike}
+
+  #TEST: The docking station gets a bike and expects bike to be working
+  it 'releases working bikes' do 
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
 
 end
